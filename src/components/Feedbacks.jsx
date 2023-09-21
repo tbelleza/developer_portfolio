ceb932f97f200452d88d8d067bfa39449db2f5d5
tbelleza <theresabelleza@gmail.com>
2023-09-20 17:22:51 -0700

    testimonails fix

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -38,13 +38,13 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
 const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
-      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-300`}>
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>what others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials</h2>
         </motion.div>
       </div>
-      <div className={`${styles.paddingX} -mt-10 pb-14 flex flex-wrap gap-7`}>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard
             key={testimonial.name}
